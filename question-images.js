656f9b184f515ec81f016f8613c022b3de8b3059
Justin Chediak <justin.chediak@gmail.com>
2026-03-24 13:14:01 -0700

    Images

---
+++ question-images.js
@@ -0,0 +1,50 @@
+export const QUESTION_IMAGES = {
+  "2024": {
+    51: [
+      {
+        src: "/question-images/2024/51-1.png",
+        alt: "Clinical photo for 2024 question 51"
+      }
+    ],
+    98: [
+      {
+        src: "/question-images/2024/98-1.png",
+        alt: "Clinical photo for 2024 question 98"
+      }
+    ],
+    143: [
+      {
+        src: "/question-images/2024/143-1.png",
+        alt: "Clinical photo 1 for 2024 question 143"
+      },
+      {
+        src: "/question-images/2024/143-2.png",
+        alt: "Clinical photo 2 for 2024 question 143"
+      }
+    ],
+    191: [
+      {
+        src: "/question-images/2024/191-1.png",
+        alt: "Clinical photo 1 for 2024 question 191"
+      },
+      {
+        src: "/question-images/2024/191-2.png",
+        alt: "Clinical photo 2 for 2024 question 191"
+      }
+    ]
+  },
+  "2025": {
+    3: [
+      {
+        src: "/question-images/2025/3-1.png",
+        alt: "X-ray for 2025 question 3"
+      }
+    ],
+    144: [
+      {
+        src: "/question-images/2025/144-1.png",
+        alt: "Clinical photo for 2025 question 144"
+      }
+    ]
+  }
+};
